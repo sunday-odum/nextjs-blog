93fe9ab075a6dbdc43bb8aac9b3ca2f96af67910
mastertech <sundayodum@gmail.com>
2021-07-20 01:14:24 +0100

    added navigaton between pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,9 +13,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Learn<a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className="title">
+          Read{' '}
+        <Link href="/posts/first-post">
+             <a>this page!</a>
+        </Link>
+       </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
